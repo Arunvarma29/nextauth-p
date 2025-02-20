2c868d2c0f39881ab6bf35467a545a972fbfda19
Arunvarma29 <varmaarun580@gmail.com>
2025-02-20 20:26:48 +0530

    Login api ready

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,61 @@
+import { connect } from '@/dbConfig/dbConfig';
+import User from '@/models/userModel';
+import { NextResponse, NextRequest } from 'next/server';
+import { sendEmail } from '@/helpers/mailer';
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv'
+
+connect()
+dotenv.config();
+
+export async function POST(request: NextRequest) {
+    try {
+        const reqBody = await request.json();
+        const { email, password } = reqBody;
+        //validation
+        console.log(reqBody);
+
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return NextResponse.json({ error: "User does not exist " },
+                { status: 400 }
+            )
+        }
+        console.log("user exist");
+
+        const validPassword = await bcryptjs.compare(password, user.password);
+
+        if (!validPassword) {
+            return NextResponse.json({ error: "check your credentials" },
+                { status: 400 })
+        }
+
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+
+        console.log(process.env.TOKEN_SECRET);
+
+        const token = jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: '1d' });
+        
+        const response= NextResponse.json({
+            message:"Logged In Success",
+            success: true
+        })
+
+        response.cookies.set("token",token,{
+            httpOnly: true
+        })
+
+        return response
+
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message },
+            { status: 500 })
+    }
+
+}
--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -11,27 +11,29 @@ export async function POST(request: NextRequest) {
         const { token } = reqBody;
         console.log(token);
 
-        const user=await User.findOne({verifyToken:token,
-            verifyTokenExpiry:{$gt:Date.now()}})
+        const user = await User.findOne({
+            verifyToken: token,
+            verifyTokenExpiry: { $gt: Date.now() }
+        })
 
-            if(!user){
-                return NextResponse.json({error:"Invalid Token"},
-                    {status:400}
-                )
-            }
-            console.log(user);
-            user.isVerified=true;
-            user.verifyToken=undefined;
-            user.verifyTokenExpiry=undefined;
+        if (!user) {
+            return NextResponse.json({ error: "Invalid Token" },
+                { status: 400 }
+            )
+        }
+        console.log(user);
+        user.isVerified = true;
+        user.verifyToken = undefined;
+        user.verifyTokenExpiry = undefined;
 
-           await user.save();
+        await user.save();
 
-           return NextResponse.json({
-            message:"Email verified successfully",
-            success:true,
-           },{status:500}
+        return NextResponse.json({
+            message: "Email verified successfully",
+            success: true,
+        }, { status: 500 }
         )
-            
+
 
     } catch (error: any) {
         return NextResponse.json({ error: error.message },
